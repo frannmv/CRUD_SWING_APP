bd5bfd6dd6e19ecd6f2bffac4d746e9223eb999a
frannmv <morenoverafranco@gmail.com>
2024-02-23 21:31:56 -0300

    Creada la interfaz formulario

---
+++ src/gui/Estudiantes.java
@@ -0,0 +1,28 @@
+package gui;
+import javax.swing.*;
+public class Estudiantes extends JFrame {
+
+    private JLabel lblTItulo;
+    private JPanel panelBotones;
+    private JButton btnConsultar;
+    private JButton btnCargarDatos;
+    private JPanel panelConsultar;
+    private JPanel panelCargarDatos;
+    private JTextField txtNombre;
+    private JTextField txtCarrera;
+    private JTextField txtEdad;
+    private JTextField txtTelefono;
+    private JTextField txtID;
+    private JTextField txtApellido;
+    private JList listaConsultar;
+    private JLabel lblID;
+    private JLabel lblNombre;
+    private JLabel lblApellido;
+    private JLabel lblEdad;
+    private JLabel lblTelefono;
+    private JLabel lblCarrera;
+
+    public Estudiantes(){
+
+    }
+}
